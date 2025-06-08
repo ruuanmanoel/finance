ff40600a1696c9a59f43b5324c6729b9f01119c5
Ruan Manoel <ruuanmanoel@gmail.com>
2025-06-08 18:23:14 -0300

    :sparkles: feat: middleware verificação de token para rotas privadas.

---
+++ middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Não autenticado: token não encontrado" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token inválido ou expirado" });
+  }
+}
+
+module.exports = verifyToken;
