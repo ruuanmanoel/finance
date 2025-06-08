d2e12e2c2f073ad3fff5894c9b3275adfecc80f6
Ruan Manoel <ruuanmanoel@gmail.com>
2025-06-08 20:11:22 -0300

    :sparkles: feat: rotas privadas e middleware

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const publicRoutes = require("./routes/public");
+const privateRoutes = require("./routes/private");
+const auth = require("./middleware/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use("/", publicRoutes);
+app.use("/", auth, privateRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("está rodando localhost:" + process.env.PORT);
