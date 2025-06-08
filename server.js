8d05944c2e5eaec1d8ffad85d11f8c5076b11463
Ruan Manoel <ruuanmanoel@gmail.com>
2025-06-08 18:24:04 -0300

    :sparkles: feat: rota para login.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const verifyToken = require("./middleware");
 
 const prisma = new PrismaClient();
 const app = express();
@@ -27,6 +29,34 @@ app.post("/cadastro", async (req, res) => {
     res.status(201).json(userWithoutPassword);
   } catch (error) {
     res.status(500).json({ error: "Erro ao cadastrar usuário" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findFirst({ where: { email } });
+    if (user === null) {
+      return res.status(201).json({ message: "Email não encontrado." });
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      return res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "Strict",
+          maxAge: 3600000,
+        })
+        .json({ message: "Logado com sucesso", token });
+    } else {
+      return res.status(201).json({ message: "Senha invalida" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Error ao buscar usuário." });
   }
 });
 
